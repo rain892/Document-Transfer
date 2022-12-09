fb323a5afca225ab217ba9c211d4d896f7b9b3b6
rain892 <33967269+rain892@users.noreply.github.com>
2022-12-08 16:24:50 -0800

    Add files via upload

---
+++ transfer.py
@@ -0,0 +1,30 @@
+import os 
+import shutil
+
+from_dir =  "F:/Users/Arin/Desktop"
+to_dir = "C:/Document_Files"
+
+list_of_files = os.listdir(from_dir)
+print(list_of_files)
+
+for file_name in list_of_files:
+    name, extension = os.path.splitext(file_name)
+    
+
+    if extension == '':
+        continue 
+    if extension in [ '.txt', '.doc', '.docx', '.pdf' ]:
+
+        path1 = from_dir + '/' + file_name
+        path2 = to_dir + '/' + "Document_Files"
+        path3 = to_dir + '/' + "Document_Files" + '/' + file_name
+
+        if os.path.exists(path2):
+            print("Moving" + file_name + "......")
+
+            shutil.move(path1,path3)
+
+        else:
+            os.makedirs(path2)
+            print("Moving" + file_name + "......")
+            shutil.move(path1,path3)
